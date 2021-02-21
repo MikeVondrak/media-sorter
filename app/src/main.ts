c9ac6e502e0cfb280eb11ebd2dab79b6e48a0f5e
Mike Vondrak <mvondrak@gmail.com>
2021-02-21 02:39:19 -0700

    refactor electron into app class

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,108 +1,42 @@
-import { app, App, BrowserWindow, dialog } from 'electron';
-import * as path from 'path';
+
 import { routes } from './routes';
-// const {app, BrowserWindow} = require('electron')
-// const path = require('path')
+import * as path from 'path';
+import { ElectronApp } from './app';
+import { dialog, ipcMain } from 'electron';
+import { AppState, defaultAppState } from './models/app.model';
 
-// for Mac app store ??
-//if (process.mas) app.setName('Media File Sorter');
-app.setName('Media File Sorter');
+let appState: AppState = defaultAppState;
 
-const debug = /--debug/.test(process.argv[2]);
-
-let mainWindow: BrowserWindow;
-
-console.log('APP RUNNING');
-debugger;
-initialize();
-console.log('APP INITIALIZED');
-debugger;
-
-function initialize() {
-
-  function createWindow() {
-    const windowOptions = {
-      show: false, // wait for ready-to-show event before rendering to avoid pop-in
-      width: 1080,
-      minWidth: 320,
-      height: 840,
-      //backgroundColor: "#000",
-      title: app.getName(),
-      webPreferences: {
-        nodeIntegration: true
-      }
-    }
-    mainWindow = new BrowserWindow(windowOptions);
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-
-    const mainPage = routes.html._root + routes.html.main;
-    console.log("Loading: " + mainPage);
-
-    mainWindow.loadURL(path.join('file://', __dirname, mainPage));
-
-    // Launch fullscreen with DevTools open, usage: npm run debug
-    if (debug) {
-      mainWindow.webContents.openDevTools();
-      mainWindow.maximize();
-      require('devtron').install();
-    }
-
-    mainWindow.on('closed', () => {
-      mainWindow = null as any;
-    })
+const windowOptions: Partial<Electron.BrowserWindowConstructorOptions> = {
+  show: false, // wait for ready-to-show event before rendering to avoid pop-in
+  width: 1080,
+  minWidth: 320,
+  height: 840,
+  //backgroundColor: "#000",
+  title: 'Media File Sorter',
+  icon: path.join(__dirname, '../', '/assets/images/media-file-sorter-icon-16.png'),
+  webPreferences: {
+    nodeIntegration: true,
+    enableRemoteModule: true 
   }
+};
 
-  makeSingleInstance();
-  attachAppEvents(app, createWindow);
-  
-  
-  // loadApp();
-}
+console.log('Initializing with window options:', windowOptions);
 
-// FROM ELECTRON DEMO
-// Make this app a single instance app.
-//
-// The main window will be restored and focused instead of a second window
-// opened when a person attempts to launch a second instance.
-//
-// Returns true if the current version of the app should quit instead of
-// launching.
-function makeSingleInstance () {
-  if (process.mas) return;
+const electronApp: ElectronApp = 
+  new ElectronApp( 
+    'Media File Sorter', // app name
+    windowOptions, // BrowserWindow config
+    routes, // routes const
+    'main' // html path to match in routes
+  );
 
-  app.requestSingleInstanceLock();
+ipcMain.on('setSourceFolder', (event, arg) => {
+  console.log('********* setSourceFolder: ' + arg);
+  appState.sourceFolder = arg;
+});
 
-  app.on('second-instance', () => {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
-    }
-  }); 
-}
-
-/**
- * Attach events for ready, activate, window close
- * @param app electron App object
- * @param createWindow function to call after window is created
- */
-function attachAppEvents(app: App, createWindow: () => void) {
-  app.on('ready', () => {
-    createWindow();
-  });
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    if (mainWindow === null) {
-      createWindow();
-    }
-  });
-}
-
-function openFolder() {
-  console.log("OPEN FOLDER");
-}
+ipcMain.on('setDestinationFolder', (event, arg) => {
+  console.log('********* setDestinationFolder: ' + arg);
+  appState.destinationFolder = arg;
+});
